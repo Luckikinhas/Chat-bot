b4aabe0a5c69d73a60e0db6e9a8a1aa993461860
Antônio Gonzaga <gonzagaantonio000@gmail.com>
2024-08-25 15:43:07 -0300

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -30,11 +30,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const prompt_3 = 'Pesquise mais aprofundado sobre cada bioma brasileiro e seus índices pluviométricos ';
     const prompt_4 = 'Fique atento com os estados e cidades que possuem cada bioma e qual sua extensão ';
     const prompt_5 = 'Foque um pouco nas caracteristicas de cada bioma e quais as principais dificuldades de preservação  ';
-    const prompt_6 = 'Minas Gerais é o estado que mais possui biomas, exeto o pantanal  ';
+    const prompt_6 = ' Os estados que possuem mias biomas são Mato Grosso, Mato Grosso do Sul, Bahia e Minas Gerais, cada um com três biomas ';
+    const prompt_7 = 'Minas  Gerais possui tres biomas o cerrado, a mata Atlântica, e a Caatinga, não possui pantanal e pampa  ' 
+    const prompt_8 = 'A Bahia possui tres biomas Mata Atlântica, Cerrado e Caatinga, não possui amazonia  ' 
+    const prompt_9 = 'Mato Grosso possui tres biomas Amazônia; Cerrado; Pantanal ' 
+    const prompt_10 = 'Mato Grosso do Sul possui tres biomas o Cerrado, a Mata Atlântica e o Pantanal. ' 
     chat.sendMessage(prompt_1);
     chat.sendMessage(prompt_2);
     chat.sendMessage(prompt_3);
     chat.sendMessage(prompt_4);
     chat.sendMessage(prompt_5);
     chat.sendMessage(prompt_6);
+    chat.sendMessage(prompt_7);
+    chat.sendMessage(prompt_8);
+    chat.sendMessage(prompt_9);
+    chat.sendMessage(prompt_10);
 })
