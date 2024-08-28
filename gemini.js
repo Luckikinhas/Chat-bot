36c8f7b63ed292d730b4bd2288a1de8b56e33abd
Luckikinhas <162851257+Luckikinhas@users.noreply.github.com>
2024-08-28 01:27:45 -0300

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 
-const API_KEY = "AIzaSyAeTp_Gi1lOOr2f779NL3LjDjpifqMd8jk";
+const API_KEY = "AIzaSyCkPVXNShiJ6p0R7zNeKypF9Kk-hpk02DQ";
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
@@ -13,6 +13,7 @@ async function mandar_mensagem(msg) {
     const result = await chat.sendMessage(msg);
     const response = await result.response;//response recebe a resposta do result
     const text = await response.text();//convert text
+    history.push(response);//SO UM TESTE PLM DE DEUS
     console.log(text);
 }
 
@@ -24,25 +25,25 @@ async function perguntarModelo() {
 const botao = document.getElementById("botao")
 botao.addEventListener("click", perguntarModelo);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const prompt_1 = "Você vai fornecer informações sobre os biomas brasileiros para alunos de uma sala de aula";
-    const prompt_2 = 'Fique atento a plurais, pois o usuário pode escrever incorretamente';
-    const prompt_3 = 'Pesquise mais aprofundado sobre cada bioma brasileiro e seus índices pluviométricos ';
-    const prompt_4 = 'Fique atento com os estados e cidades que possuem cada bioma e qual sua extensão ';
-    const prompt_5 = 'Foque um pouco nas caracteristicas de cada bioma e quais as principais dificuldades de preservação  ';
-    const prompt_6 = ' Os estados que possuem mias biomas são Mato Grosso, Mato Grosso do Sul, Bahia e Minas Gerais, cada um com três biomas ';
-    const prompt_7 = 'Minas  Gerais possui tres biomas o cerrado, a mata Atlântica, e a Caatinga, não possui pantanal e pampa  ' 
-    const prompt_8 = 'A Bahia possui tres biomas Mata Atlântica, Cerrado e Caatinga, não possui amazonia  ' 
-    const prompt_9 = 'Mato Grosso possui tres biomas Amazônia; Cerrado; Pantanal ' 
-    const prompt_10 = 'Mato Grosso do Sul possui tres biomas o Cerrado, a Mata Atlântica e o Pantanal. ' 
-    chat.sendMessage(prompt_1);
-    chat.sendMessage(prompt_2);
-    chat.sendMessage(prompt_3);
-    chat.sendMessage(prompt_4);
-    chat.sendMessage(prompt_5);
-    chat.sendMessage(prompt_6);
-    chat.sendMessage(prompt_7);
-    chat.sendMessage(prompt_8);
-    chat.sendMessage(prompt_9);
-    chat.sendMessage(prompt_10);
+document.addEventListener('DOMContentLoaded', async function comecar_cod(){
+    const allPrompts = [];
+    allPrompts.push('Vou passar algumas dicas e informações necessárias para você responder as perguntas: ')
+    allPrompts.push('você vai fornecer informações sobre os biomas brasileiros para alunos de uma sala de aula, ');
+    allPrompts.push('fique atento a plurais, pois o usuário pode escrever incorretamente, ');
+    allPrompts.push('pesquise mais aprofundado sobre cada bioma brasileiro e seus índices pluviométricos, ');
+    allPrompts.push('fique atento com os estados e cidades que possuem cada bioma e qual sua extensão, ');
+    allPrompts.push('foque um pouco nas caracteristicas de cada bioma e quais as principais dificuldades de preservação, ');
+    allPrompts.push('os estados que possuem mias biomas são Mato Grosso, Mato Grosso do Sul, Bahia e Minas Gerais, cada um com três biomas, ');
+    allPrompts.push('Minas  Gerais possui tres biomas o cerrado, a mata Atlântica, e a Caatinga, não possui pantanal e pampa, ');
+    allPrompts.push('a Bahia possui tres biomas Mata Atlântica, Cerrado e Caatinga, não possui amazonia, ');
+    allPrompts.push('Mato Grosso possui tres biomas Amazônia; Cerrado; Pantanal, ');
+    allPrompts.push('Mato Grosso do Sul possui tres biomas o Cerrado, a Mata Atlântica e o Pantanal, '); 
+
+    console.log('Começo')
+    let mandar_prompt = await chat.sendMessage(allPrompts);//TESTE
+    await mandar_prompt.response;//TESTE
+    console.log('Fim')
+
+    document.createElement('button');
+
 })
