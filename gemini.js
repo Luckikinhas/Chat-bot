36eafd4fdde153334296607377604a63bf6370a3
Diogguin <diogosouzalmeida2@gmail.com>
2024-09-24 14:35:50 -0300

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -2,8 +2,21 @@ let quantidadeLetrasDivResp = 0;
 let quantidadeLetrasDivPerg = 0;
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 
-const API_KEY = "AIzaSyCkPVXNShiJ6p0R7zNeKypF9Kk-hpk02DQ";
+const ajuda_API_KEY = Math.floor(Math.random() * 2);
+console.log(`ajuda_API_KEY = ${ajuda_API_KEY}`)
+function choose_API_key ()
+{
+    if (ajuda_API_KEY==0)
+    {
+        return "AIzaSyCkPVXNShiJ6p0R7zNeKypF9Kk-hpk02DQ";
+    }
 
+    else if (ajuda_API_KEY==1)
+    {
+        return "AIzaSyCoRaDKAwXffGSdkCxjwgynSjIsZrPaIDc";
+    }
+}
+const API_KEY = choose_API_key();
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
@@ -163,28 +176,47 @@ async function perguntarModelo() {
 
 document.addEventListener('DOMContentLoaded', async function comecar_cod(){
     const allPrompts = [];
-    allPrompts.push('Vou passar algumas dicas e informações necessárias para você responder as perguntas:')
-    allPrompts.push('seu nome vai ser Ifinho.')//pode mudar o nome se quiser era só um teste
-    allPrompts.push('mande a mensagem de escolhar o icone depois que o usuario falar seu nome ')
-    allPrompts.push('sempre que alguem perguntar qual sua missão aqui sua resposta vai ser: é fornecer informações sobre os biomas brasileiros para alunos.');
-    allPrompts.push('fique atento a plurais, pois o usuário pode escrever incorretamente.');
-    allPrompts.push('pesquise mais profundamente sobre cada bioma brasileiro e seus índices pluviométricos.');
-    allPrompts.push('fique atento com os estados e cidades que possuem cada bioma e qual sua extensão.');
-    allPrompts.push('foque um pouco nas caracteristicas de cada bioma e quais as principais dificuldades de preservação.');
-    allPrompts.push('sempre que for perguntado quais estados possuem mais biomas a resposta é: os estados que possuem mais biomas são Mato Grosso, Mato Grosso do Sul, Bahia e Minas Gerais, cada um com três biomas.');
-    allPrompts.push('Minas Gerais possui três biomas o cerrado, a mata Atlântica, e a Caatinga.');
-    allPrompts.push('a Bahia possui três biomas Mata Atlântica, Cerrado e Caatinga.');
-    allPrompts.push('Mato Grosso possui três biomas Amazônia, Cerrado e Pantanal.');
-    allPrompts.push('Mato Grosso do Sul possui três biomas o Cerrado, a Mata Atlântica e o Pantanal.'); 
+    allPrompts.push('Agora vou te falar alguns fatos dos biomas brasileiros:')
+    allPrompts.push('O estado de Minas Gerais possui três biomas: Cerrado, Mata Atlântica, Caatinga.');//MG 1
+    allPrompts.push('O estado de Bahia possui três biomas: Mata Atlântica, Cerrado e Caatinga.');//Bahia 2
+    allPrompts.push('O estado de Mato Grosso possui três biomas: Amazônia, Cerrado e Pantanal.');//Mato Grosso 3
+    allPrompts.push('O estado de Mato Grosso do Sul possui três biomas o Cerrado: Mata Atlântica e o Pantanal.');//Mato Grosso do Sul 4 
+    allPrompts.push('O estado de São Paulo possui dois Biomas: Mata Atlântica e Cerrado.');//SP 5
+    allPrompts.push('O estado de Rio de Janeiro possui um bioma: Mata Atlântica.');//RJ 6
+    allPrompts.push('O estado de Espírito Santo possui um bioma: Mata Atlântica.');//ES 7
+    allPrompts.push('O estado de Paraná possui dois Biomas: Mata Atlântica e Cerrado.');//Paraná 8
+    allPrompts.push('O estado de Santa Catarina possui um bioma: Mata Atlântica.');//SC 9
+    allPrompts.push('O estado de Amapá possui apenas um bioma: Amazônia.');//Amapá 10
+    allPrompts.push('O estado de Roraima possui apenas um bioma: Amazônia.');//RR 11
+    allPrompts.push('O estado de Pará possui a apenas dois bioma: Amazônia e o Cerrado.');//Pará 12
+    allPrompts.push('O estado de Amazonas possui um bioma: Amazônia.');//Amazonas 13
+    allPrompts.push('O estado de Rondônia possui um bioma: Amazônia.');//RN 14
+    allPrompts.push('O estado de Acre possui um bioma: Amazônia.');//Acre 15
+    allPrompts.push('O estado de Tocantins possui dois biomas: Cerrado e Amazônia.');//TO 16
+    allPrompts.push('O estado de Goiás possui dois biomas: Cerrado e Mata Atlântica.');//GO 17
+    allPrompts.push('O estado de Alagoas possui 2 biomas: Mata Atlântica e Caatinga.');//AL 18
+    allPrompts.push('O estado do Ceará possui 2 biomas: Caatinga e Mata Atlântica.');//Ceará 19
+    allPrompts.push('O estado do Maranhão possui 3 biomas: Amazônia, Cerrado e Caatinga.');//Maranhao 20
+    allPrompts.push('O estado da Paraíba possui 2 biomas: Caatinga e Mata Atlântica.');//Paraiba 21
+    allPrompts.push('O estado de Pernambuco possui 2 biomas: Caatinga e Mata Atlântica.');//Pernambuco 22
+    allPrompts.push('O estado do Piauí possui 2 biomas: Caatinga e Cerrado.');//Piaui 23
+    allPrompts.push('O estado do Rio Grande do Norte possui 2 biomas: Caatinga e Mata Atlântica.');//RGN 24
+    allPrompts.push('O estado de Sergipe possui 2 biomas: Caatinga e Mata Atlântica.')//Sergipe 25
+    allPrompts.push('O estado do Rio Grande do Sul possui dois biomas: Pampa e Mata Atlântica')//RGS 26
 
+    allPrompts.push('Agora que acabamos com os biomas brasileiros, vamos falar sobre alguns comportamentos, características dicas e informações necessárias que você deve seguir:')
+    allPrompts.push('seu nome vai ser Ifinho.')//pode mudar o nome se quiser era só um teste
+    allPrompts.push('sempre que alguem perguntar qual sua missão aqui sua resposta vai ser: é fornecer informações sobre os biomas brasileiros para alunos.');
+    allPrompts.push('fique atento com plurais, pois o usuário pode escrever incorretamente.');
+    allPrompts.push('pesquise mais profundamente sobre cada bioma brasileiro');
+    allPrompts.push('fique atento com os estados e cidades que possuem cada bioma e qual sua extensão.');
+    allPrompts.push('sempre que for perguntado quais estados possuem mais biomas a resposta é: os estados que possuem mais biomas são Mato Grosso, Mato Grosso do Sul, Bahia e Minas Gerais, cada um com três biomas.');
+    allPrompts.push('Tente resumir as suas respostas');
+    
     console.log('Começo');
     await mandar_mensagem('Você pode se apresentar pra mim e dizer qual sua missão? Após isso, pergunte meu nome');
     await chat.sendMessage(allPrompts);
 
-    
-
-
-   
     console.log('Fim');
 
     const botao = document.getElementById("botao");
@@ -195,4 +227,3 @@ function funcaoTeste(){
     mostrarPerguntaUsuario('isso é uma mensagem de teste', iconeAtual)
     
 }
-
